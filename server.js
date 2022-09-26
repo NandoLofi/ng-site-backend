007c9297de3a2093dea984bd088d59843f737b07
Fernando Galvan <n.brighter00@gmail.com>
2022-09-26 15:41:37 -0500

    added connection

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const { config } = require('dotenv')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
+const mongoose = require('mongoose')
+const DATABASE_URI = process.env.DATABASE_URI
+
+const db = mongoose.connection
+mongoose.connect(DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+db.once('open', ()=>{
+    console.log("Mongo connected")
+})
+
 
 app.get("/", (req, res) =>{
     res.send("Welcome Home")
