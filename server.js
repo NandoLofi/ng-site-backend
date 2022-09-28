02f948673fec4bfbaa586577d604516fca662cf3
Fernando Galvan <n.brighter00@gmail.com>
2022-09-28 14:11:33 -0500

    trying to debug nodemailer issue

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const mongoose = require('mongoose')
 const DATABASE_URI = process.env.DATABASE_URI
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const EMAIL = process.env.EMAIL
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+
+app.use(express.json())
+app.use(cors())
+
 
 const db = mongoose.connection
 mongoose.connect(DATABASE_URI, {
@@ -15,9 +23,29 @@ db.once('open', ()=>{
     console.log("Mongo connected")
 })
 
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: EMAIL,
+        pass: EMAIL_PASSWORD
+    }
+})
+contactEmail.verify((error) =>{
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Rady to Send")
+    }
+})
+
+
 
 app.get("/", (req, res) =>{
     res.send("Welcome Home")
 })
+app.post('/request/new', (req, res) => {
+    const newInfo = req.body
+})
+
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`))
